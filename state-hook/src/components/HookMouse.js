ff7571b219556812f4f712920ddd8ae1834049ac
Aditya Neve <aditya.neve@gmail.com>
2021-11-14 23:46:23 +0530

    useEffect with cleanup
    * When you want to execute some component cleanup code, you include it in a function and return that function, from the function passed to useEffect.
    * The clean up code can be cancelling subscriptions, timers or removing event handlers.
    * useEffect(() => {
            console.log("useEffect called");
            window.addEventListener("mousemove", logMousePosition);
    
            // Cleanup Code
            return () => {
                console.log("Component unmounting code");
                window.removeEventListener("mousemove", logMousePosition);
            };
        }, []);

--- state-hook/src/components/HookMouse.js
+++ state-hook/src/components/HookMouse.js
@@ -1,20 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function HookMouse() {
-
-    const [x, setX] = useState(0)
-    const [y, setY] = useState(0)
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
 
     const logMousePosition = (e) => {
         console.log("Mouse event");
         setX(e.clientX);
         setY(e.clientY);
-    }
+    };
 
     useEffect(() => {
         console.log("useEffect called");
         window.addEventListener("mousemove", logMousePosition);
-    }, [])
+
+        // Cleanup Code
+        return () => {
+            console.log("Component unmounting code");
+            window.removeEventListener("mousemove", logMousePosition);
+        };
+    }, []);
 
     return (
         <div>
@@ -22,7 +27,7 @@ function HookMouse() {
                 Hooks X: {x} Y: {y}
             </h2>
         </div>
-    )
+    );
 }
 
-export default HookMouse
+export default HookMouse;
