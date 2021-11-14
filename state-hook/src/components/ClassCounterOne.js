e7ee6a9235a803df415971c294f471e615d46bfa
Aditya Neve <aditya.neve@gmail.com>
2021-11-14 19:18:47 +0530

    Conditionally run effects
    * useEffect hook is called after every single render.
    * It will create a performance problem.
    * In order to conditionally run an effect specify the second parameter to use effect
    * useEffect(() => {
            console.log("useEffect: updating document title");
            document.title = `Your clicked ${count} times`;
        }, [count]);

--- state-hook/src/components/ClassCounterOne.js
+++ state-hook/src/components/ClassCounterOne.js
@@ -5,8 +5,9 @@ class ClassCounterOne extends Component {
         super(props);
 
         this.state = {
-            count: 0
-        }
+            count: 0,
+            name: "",
+        };
     }
 
     componentDidMount() {
@@ -14,18 +15,30 @@ class ClassCounterOne extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        document.title = `Clicked ${this.state.count} times`
+        if (prevState.count !== this.state.count) {
+            console.log("Updating document title");
+            document.title = `Clicked ${this.state.count} times`;
+        }
     }
 
     render() {
         return (
             <div>
-                <button onClick={() => this.setState({ count: this.state.count + 1})}>
+                <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={(e) => this.setState({ name: e.target.value })}
+                />
+                <button
+                    onClick={() =>
+                        this.setState({ count: this.state.count + 1 })
+                    }
+                >
                     Click {this.state.count} times
                 </button>
             </div>
-        )
+        );
     }
 }
 
-export default ClassCounterOne
+export default ClassCounterOne;
