7b9a90d2ddaf13735bd2b2e88529ccd0aedc75d1
Aditya Neve <aditya.neve@gmail.com>
2021-11-15 19:44:52 +0530

    used: createContext() :: useContext Hook Part-2
    * Code works, But not most readable code.
    * The best way is to use useContext Hook.

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,36 +1,45 @@
-import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterTwo from './components/HookCounterTwo';
-import ClassCounterOne from './components/ClassCounterOne';
-import HookCounterOne from './components/HookCounterOne';
-import ClassMouse from './components/ClassMouse';
-import HookMouse from './components/HookMouse';
-import MouseContainer from './components/MouseContainer';
-import IntervalClassCounter from './components/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
-import DataFetching from './components/DataFetching';
+import "./App.css";
+import ClassCounter from "./components/ClassCounter";
+import HookCounter from "./components/HookCounter";
+import HookCounterFour from "./components/HookCounterFour";
+import HookCounterThree from "./components/HookCounterThree";
+import HookCounterTwo from "./components/HookCounterTwo";
+import ClassCounterOne from "./components/ClassCounterOne";
+import HookCounterOne from "./components/HookCounterOne";
+import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
+import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
+import React from "react";
+import ComponentC from "./components/ComponentC";
 
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 function App() {
-  return (
-    <div className="App">
-      <DataFetching />
-      {/* <IntervalHookCounter /> */}
-      {/* <IntervalClassCounter /> */}
-      {/* <MouseContainer /> */}
-      {/* <HookMouse /> */}
-      {/* <ClassMouse /> */}
-      {/* <HookCounterOne /> */}
-      {/* <ClassCounterOne /> */}
-      {/* <HookCounterFour /> */}
-      {/* <HookCounterThree /> */}
-      {/* <HookCounterTwo /> */}
-      {/* <HookCounter /> */}
-      {/* <ClassCounter /> */}
-    </div>
-  );
+    return (
+        <div className="App">
+            <UserContext.Provider value={"Aditya"}>
+              <ChannelContext.Provider value={"Neve"}>
+                <ComponentC />
+              </ChannelContext.Provider>
+            </UserContext.Provider>
+            {/* <DataFetching /> */}
+            {/* <IntervalHookCounter /> */}
+            {/* <IntervalClassCounter /> */}
+            {/* <MouseContainer /> */}
+            {/* <HookMouse /> */}
+            {/* <ClassMouse /> */}
+            {/* <HookCounterOne /> */}
+            {/* <ClassCounterOne /> */}
+            {/* <HookCounterFour /> */}
+            {/* <HookCounterThree /> */}
+            {/* <HookCounterTwo /> */}
+            {/* <HookCounter /> */}
+            {/* <ClassCounter /> */}
+        </div>
+    );
 }
 
 export default App;
