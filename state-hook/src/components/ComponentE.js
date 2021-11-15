b89755428f89564c2c4f03e70732c364fd968ad6
Aditya Neve <aditya.neve@gmail.com>
2021-11-15 19:55:08 +0530

    useContext Hook Part-3
    * Context provides a way to pass data through the component tree without having to pass props down manually at every level.

--- state-hook/src/components/ComponentE.js
+++ state-hook/src/components/ComponentE.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import ComponentF from './ComponentF'
+import React, { useContext } from "react";
+import ComponentF from "./ComponentF";
+import { UserContext, ChannelContext } from "../App";
 
 function ComponentE() {
+
+    const user = useContext(UserContext);
+    const channel = useContext(ChannelContext);
+
     return (
         <div>
+
+            <h1>
+                Name: {user} {" "} Channel: {channel}
+
+            </h1>
+
             <ComponentF />
         </div>
-    )
+    );
 }
 
-export default ComponentE
+export default ComponentE;
