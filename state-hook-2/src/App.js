b77ec087169e23cd9db9bdb3ef7e25479e7788fe
Aditya Neve <aditya.neve@gmail.com>
2021-11-17 00:14:58 +0530

    useMemo Hook
    ** Difference between useCallback & useMemo **
    * useCallback will cache the provided function instance itself.
    * useMemo invokes the provided  function and caches its result.

--- state-hook-2/src/App.js
+++ state-hook-2/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './components/ParentComponent';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      <Counter />
+      {/* <ParentComponent /> */}
     </div>
   );
 }
