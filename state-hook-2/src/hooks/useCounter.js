8705494d4b4fc53aff4efcb087f5649c5661104e
Aditya Neve <aditya.neve@gmail.com>
2021-11-17 13:52:44 +0530

    useCounter Custom Hook

---
+++ state-hook-2/src/hooks/useCounter.js
@@ -0,0 +1,21 @@
+import {useState} from 'react'
+
+function useCounter(initialCount = 0) {
+    const [count, setCount] = useState(initialCount, incrementBy);
+
+    const increment = () => {
+        setCount((prevCount) => prevCount + incrementBy);
+    };
+
+    const decrement = () => {
+        setCount((prevCount) => prevCount - incrementBy);
+    };
+
+    const reset = () => {
+        setCount(0);
+    };
+
+    return [count, increment, decrement, reset];
+}
+
+export default useCounter
