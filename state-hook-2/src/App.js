d8cf6098db6440683149c177a72597da3e668626
Aditya Neve <aditya.neve@gmail.com>
2021-11-17 13:12:11 +0530

    useRef Hook Part-2

--- state-hook-2/src/App.js
+++ state-hook-2/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import ParentComponent from './components/ParentComponent';
 import Counter from './components/Counter';
 import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput />
+      <HookTimer />
+      <ClassTimer />
+      {/* <FocusInput /> */}
       {/* <Counter /> */}
       {/* <ParentComponent /> */}
     </div>
