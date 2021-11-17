198ecf4a2049c78e9a3703b16c3345ce8fc02c7f
Aditya Neve <aditya.neve@gmail.com>
2021-11-17 10:36:14 +0530

    useRef Hook
    * This Hook makes it possible to access dom nodes directly within functional components.

--- state-hook-2/src/App.js
+++ state-hook-2/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './components/ParentComponent';
 import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <FocusInput />
+      {/* <Counter /> */}
       {/* <ParentComponent /> */}
     </div>
   );
