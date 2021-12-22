5c666312e8989000ff6359bd2c47856067513df0
Aditya Neve <aditya.neve@gmail.com>
2021-12-22 19:19:19 +0530

    webpack notes-app

---
+++ webpack2/src/index.js
@@ -0,0 +1,50 @@
+import('./index.css');
+
+/* 
+Create a simple note taking web application with HTML/CSS/JS
+user should be able take a simple note, and it should be seen on ui.
+application should have a logo at top. import this logo in JS. bundle it through webpack
+Bundle the other things like CSS, (fonts or icons if you have) with Webpack.
+final output should work with bundled code from build folder
+
+*/
+
+let root = document.getElementById('root');
+
+    let mainDiv = document.createElement('div');
+    mainDiv.setAttribute('id', 'mainDiv');
+
+    let inputField = document.createElement('input');
+    inputField.setAttribute('id', 'inputField');
+
+    let label = document.createElement('label');
+    label.setAttribute('for', 'inputField');
+    label.innerText = 'Note: ';
+
+    let submitBtn = document.createElement('button');
+    submitBtn.innerText = 'SUBMIT';
+
+    let resetBtn = document.createElement('button');
+    resetBtn.innerText = 'RESET';
+
+mainDiv.append(label, inputField, submitBtn, resetBtn);
+
+let notesDiv = document.createElement('div');
+notesDiv.setAttribute('id', 'notesDiv');
+
+submitBtn.addEventListener('click', () => {
+    let note = document.createElement('h2');
+    note.innerText = inputField.value;
+
+    notesDiv.append(note);
+    inputField.value = null;
+    inputField.focus();
+});
+
+resetBtn.addEventListener('click', () => {
+    notesDiv.innerHTML = null;
+    inputField.value = null;
+    inputField.focus();
+});
+
+root.append(mainDiv, notesDiv);
