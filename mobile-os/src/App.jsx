c39dc029d0115a14ece41cb3270d4929ce157c0c
Aditya Neve <aditya.neve@gmail.com>
2021-12-23 20:45:54 +0530

    background color added using material ui

--- mobile-os/src/App.jsx
+++ mobile-os/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+
+import { useStyles } from './styles';
 import { List } from './components';
 
 function App() {
+    const classes = useStyles();
     const mobileOS = ['Android', 'BlackBerry', 'iPhone', 'Windows Phone'];
     const mobileManufactures = ['Samsung', 'HTC', 'Micromax', 'Apple'];
 
     return (
         <div className='App'>
-            <header>
+            <header className={classes.root}>
                 <h1>Mobile Operating System</h1>
                 <List props={mobileOS} />
 
---
+++ mobile-os/src/styles/useStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    },
+});
+
+export default useStyles;
