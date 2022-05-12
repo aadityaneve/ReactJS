a6b25047ae998cc438afd48c1b208932783f4e2a
Aditya Neve <aditya.n@tricog.com>
2022-05-12 12:03:05 +0530

    basic react-query

---
+++ react-query/src/App.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import logo from './logo.svg';
+import './App.css';
+import SuperHeroesPage from './components/SuperHeroes.page';
+import HomePage from './components/Home.page';
+import RQSuperHeroesPage from './components/RQSuperHeroes.page';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/super-heroes'>
+              <SuperHeroesPage />
+            </Route>
+            <Route path='/rq-super-heroes'>
+              <RQSuperHeroesPage />
+            </Route>
+            <Route path='/'>
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
