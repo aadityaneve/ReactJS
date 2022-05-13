0de1a71c1dbbfe3178c092fd19a2d331da9a49c2
Aditya Neve <aditya.n@tricog.com>
2022-05-13 15:46:16 +0530

    Mutation

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -20,65 +20,63 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/super-heroes'>Traditional Super Heroes</Link>
-              </li>
-              <li>
-                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-              </li>
-              <li>
-                <Link to='/rq-parallel'>Parallel Query</Link>
-              </li>
-              <li>
-                <Link to='/rq-dynamic-parallel'>Dynamic Parallel Query</Link>
-              </li>
-              <li>
-                <Link to='/rq-dependant'>Dependant Query</Link>
-              </li>
-              <li>
-                <Link to='/rq-paginated'>Paginated Query</Link>
-              </li>
-              <li>
-                <Link to='/rq-infinite'>Infinite Query</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path='/rq-infinite'>
-              <InfiniteQueriesPage />
-            </Route>
-            <Route path='/rq-paginated'>
-              <PaginatedQueriesPage />
-            </Route>
-            <Route path='/rq-dependant'>
-              <DependantQueriesPage email='vishwas@example.com' />
-            </Route>
-            <Route path='/rq-dynamic-parallel'>
-              <DynamicParallelPage heroIds={[1, 3]} />
-            </Route>
-            <Route path='/rq-parallel'>
-              <ParallelQueriesPage />
-            </Route>
-            <Route path='/rq-super-heroes/:heroId'>
-              <RQSuperHeroPage />
-            </Route>
-            <Route path='/super-heroes'>
-              <SuperHeroesPage />
-            </Route>
-            <Route path='/rq-super-heroes'>
-              <RQSuperHeroesPage />
-            </Route>
-            <Route path='/'>
-              <HomePage />
-            </Route>
-          </Switch>
-        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-parallel'>Parallel Query</Link>
+            </li>
+            <li>
+              <Link to='/rq-dynamic-parallel'>Dynamic Parallel Query</Link>
+            </li>
+            <li>
+              <Link to='/rq-dependant'>Dependant Query</Link>
+            </li>
+            <li>
+              <Link to='/rq-paginated'>Paginated Query</Link>
+            </li>
+            <li>
+              <Link to='/rq-infinite'>Infinite Query</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/rq-infinite'>
+            <InfiniteQueriesPage />
+          </Route>
+          <Route path='/rq-paginated'>
+            <PaginatedQueriesPage />
+          </Route>
+          <Route path='/rq-dependant'>
+            <DependantQueriesPage email='vishwas@example.com' />
+          </Route>
+          <Route path='/rq-dynamic-parallel'>
+            <DynamicParallelPage heroIds={[1, 3]} />
+          </Route>
+          <Route path='/rq-parallel'>
+            <ParallelQueriesPage />
+          </Route>
+          <Route path='/rq-super-heroes/:heroId'>
+            <RQSuperHeroPage />
+          </Route>
+          <Route path='/super-heroes'>
+            <SuperHeroesPage />
+          </Route>
+          <Route path='/rq-super-heroes'>
+            <RQSuperHeroesPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
       </Router>
       <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
     </QueryClientProvider>
--- react-query/src/components/RQSuperHeroes.page.js
+++ react-query/src/components/RQSuperHeroes.page.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import { useSuperHeroesData } from '../hooks/useSuperHeroesData'
+import React, { useState } from 'react'
+import { useAddSuperHeroData, useSuperHeroesData } from '../hooks/useSuperHeroesData'
 import { Link } from 'react-router-dom'
 
 const RQSuperHeroesPage = () => {
+
+    const [name, setName] = useState('')
+    const [alterEgo, setAlterEgo] = useState('')
 
     const onSuccess = (data) => {
         console.log('Perform side effect after data fetching.', data)
@@ -17,6 +20,14 @@ const RQSuperHeroesPage = () => {
     const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError)
     console.log({ isLoading, isFetching })
 
+    const { mutate } = useAddSuperHeroData()
+
+    const handleAddHeroClick = () => {
+        console.log({ name, alterEgo })
+        const hero = { name, alterEgo }
+        mutate(hero)
+    }
+
     if (isLoading) {
         return <h2>Loading....</h2>
     }
@@ -28,6 +39,20 @@ const RQSuperHeroesPage = () => {
     return (
         <>
             <h2>RQ Super Heroes Page</h2>
+            <div>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    value={alterEgo}
+                    onChange={(e) => setAlterEgo(e.target.value)}
+                />
+                <button onClick={handleAddHeroClick}>Add Hero</button>
+            </div>
+
             <button onClick={refetch}>Fetch Heroes</button>
             {
                 data?.data.map((hero) => {
