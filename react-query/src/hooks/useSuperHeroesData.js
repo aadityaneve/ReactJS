eb3c2646f687efa43d39db9048985fd2b4a2b1da
Aditya Neve <aditya.n@tricog.com>
2022-05-13 16:01:55 +0530

    handling Mutation Response

--- react-query/src/hooks/useSuperHeroesData.js
+++ react-query/src/hooks/useSuperHeroesData.js
@@ -44,8 +44,14 @@ export const useAddSuperHeroData = () => {
     const queryClient = useQueryClient()
 
     return useMutation(addSuperHero, {
-        onSuccess: () => {
-            queryClient.invalidateQueries('super-heroes')
+        onSuccess: (data) => {
+            // queryClient.invalidateQueries('super-heroes')
+            queryClient.setQueryData('super-heroes', (oldQueryData) => {
+                return {
+                    ...oldQueryData,
+                    data: [...oldQueryData.data, data.data],
+                }
+            })
         }
     })
 }
